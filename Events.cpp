2d779f4e99ae942125d455926d03ae00d1969080
Mohannad Emad <MOHANNAD.EALI@gmail.com>
2025-09-13 00:36:02 -0600

    class

--- Events.cpp
+++ Events.cpp
@@ -6,6 +6,10 @@ Event::Event(const std::string& name, const std::string& date)
 void Event::display() const {
     std::cout << "Event: " << EventName << ", Date: " << EventDate << std::endl;
 }
+void Event::getEvent(std::string& name, std::string& date) {
+        name = EventName;
+        date = EventDate;
+  }
 
 void Event::setEvent(const std::string& name, const std::string& date) {
         EventName = name;
